88bc450c74e11045bfa78385636b96f0d99d1610
chibi723 <chibitarou.0723@gmail.com>
2025-11-29 14:27:08 +0900

    商品出品機能
    ＃ What
    商品出品機能
    ＃ Why
    商品の情報が保存できる
    
    ログイン状態の場合は、商品出品ページへ遷移できる動画
    https://gyazo.com/4930a94a41eb736e1c87d4f4a6097c49
    
    価格が入力されると同時に、販売手数料と販売利益が表示される動画
    https://gyazo.com/6c2cd5a738b727534ca3e8ed386c6735
    
    必要な情報を適切に入力して「出品する」ボタンを押すと、商品情報がデータベースに保存される動画
    https://gyazo.com/3c6c4f5e09d0de58165ea61c0794d6ad
    
    入力に問題がある状態で「出品する」ボタンが押された場合、情報は保存されず、出品ページに戻りエラーメッセージが表示される動画
    https://gyazo.com/1b76fa132718917365de2826705efb71
    
    ログアウト状態の場合は、商品出品ページへ遷移しようとすると、ログインページへ遷移する動画
    https://gyazo.com/1f299a0b346500d269cf71012fc9e67e
    
    テスト結果の画像
    https://gyazo.com/786522ac421c2ab3bd4398970665f5a1

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,47 @@
+class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    # トップページの処理
+  end
+
+  # 出品フォームを表示するためのアクション
+  def new
+    # ビューでフォームを構築するための空のインスタンス
+    @product = Product.new
+  end
+
+  # フォームから送られたデータを保存するためのアクション
+  def create
+    @product = Product.new(product_params)
+    
+    # バリデーションチェックとデータベースへの保存
+    if @product.save
+      # 保存成功時：トップページへリダイレクト
+      redirect_to root_path
+    else
+      # 保存失敗時：入力内容を保持したまま出品ページに戻す
+      # status: :unprocessable_entity はエラー時の再描画に必要なステータス
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # 安全にデータを受け取るためのストロングパラメータ
+  def product_params
+    # permit: フォームで入力されたすべての属性とActiveHashのIDを許可
+    # merge: フォームには含まれないuser_idをログインユーザーから取得して追加
+    params.require(:product).permit(
+      :image, 
+      :name, 
+      :info, 
+      :category_id, 
+      :sales_status_id, 
+      :shipping_fee_status_id, 
+      :prefecture_id, 
+      :scheduled_delivery_id, 
+      :price
+    ).merge(user_id: current_user.id)
+  end
+end
---
+++ db/migrate/20251128120152_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :info
+      t.integer :price
+      t.integer :category_id
+      t.integer :sales_status_id
+      t.integer :shipping_fee_status_id
+      t.integer :prefecture_id
+      t.integer :scheduled_delivery_id
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
