f42083e2ad7a66f731293f05cb540d6de006d675
chibi723 <chibitarou.0723@gmail.com>
2025-11-30 20:54:06 +0900

    レビュー指摘対応

--- app/controllers/items_controller.rb
+++
@@ -1,6 +0,0 @@
-class ItemsController < ApplicationController
-  def index
-    # 出品された商品を新しい順に取得
-    @products = Product.order(created_at: :desc)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'items#index'   # トップページ（一覧）
+  root to: 'products#index'   # トップページ（一覧）
 
   resources :products, only: [:index, :new, :create]
 end
